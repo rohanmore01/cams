5f7e278ec426f0f605a2475983c49f33e55b18e8
rohanmore01 <morerohan11007@gmail.com>
2024-06-16 09:54:09 +0530

    changes in config file

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'mysql',
 	'username' => 'admin',
 	'password' => 'Admin@1234',
 	'database' => 'cams',
